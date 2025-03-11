2df39732dcf9c5d592fb58c917382eaf58884840
sh2-106angel <sh2-106.angel@protonmail.com>
2025-03-11 16:54:59 +0800

    Create tabs.js

---
+++ tabs.js
@@ -0,0 +1,20 @@
+<script>
+  const tabs = document.querySelector('.tabs');
+  const tabButtons = tabs.querySelectorAll('[role="tabs"]');
+  const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+  
+  tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
+  function handleTabClick(event) {
+  event.currentTarget.setAttribute('aria-selected', true);
+  tabButtons.forEach(tab => {
+    // tab.ariaSelected = false;
+    tab.setAttribute('aria-selected', false);
+  });
+  event.currentTarget.setAttribute('aria-selected', true);
+  //finding associate tab panel
+  const { id } = event.currentTarget;
+  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  console.log(tabPanel);
+  tabPanel.hidden = false;
+
+</script>
