a65015d0bb71a6d92cfdb12f97d1397c8b954184
sh2-106angel <sh2-106.angel@protonmail.com>
2025-03-13 00:02:54 +0800

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,6 +1,6 @@
 
-  const tabButtons = document.querySelectorAll('[button role="tab"]');
-  const tabPanels = document.querySelectorAll('[div role="tabpanel"]');
+  const tabButtons = document.querySelectorAll('button[role="tab"]');
+  const tabPanels = document.querySelectorAll('div[role="tabpanel"]');
 console.log()
   function handleTabClick(event) {
   console.log(event);
@@ -22,4 +22,21 @@ debugger;
   const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
   console.log(tabPanel);
   tabPanel.hidden = false;
+     //    <menu role="tablist">
+     //      <button role="tab" id="Angel" aria-selected="true">Angel</button>
+     //      <button role="tab" id="SweetTime" aria-selected="false">Sweet Time</button>
+     //    </menu>
+ 
+      
+     //  <div class="window" role="tabpanel" aria-labelledby="Angel">
+     //    <div class="window-body">
+     //       <p>Hello! This is a work in progress...</p>
+     //    </div>
+     //  </div>
+     // <div class="window" role="tabpanel" aria-labelledby="SweetTime" hidden>
+     //   <div class="window-body"> 
+     //     <p>Hello again!</p></div>
+     // </div>
+     // </div>
+
 
