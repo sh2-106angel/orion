946721cbfc59f8cbf51c09ed1e5e5e2ecbbfb1c2
sh2-106angel <sh2-106.angel@protonmail.com>
2025-03-13 00:29:24 +0800

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -8,7 +8,7 @@ console.log()
    //mark clicked tab as selected
   event.currentTarget.setAttribute('aria-selected', true);
   const { id } = event.currentTarget;
-  const tabPanel = document.querySelector('div.window[aria-labelledby="${id}"]');
+  const tabPanel = document.querySelector('div[aria-labelledby="${id}"]');
   console.log(tabPanel);
   tabPanel.hidden = false;
 }
