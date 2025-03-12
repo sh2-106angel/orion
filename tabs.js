5404c5226cb47ee3d6df11a72cc3f90d562b524e
sh2-106angel <sh2-106.angel@protonmail.com>
2025-03-12 22:41:12 +0800

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -8,19 +8,19 @@
   console.log(event);
 }
 
-   tabButtons.forEach(button => {
-    button.addEventListener('click', handleTabClick);
-  });
-  event.currentTarget.setAttribute('aria-selected', true);
-  tabButtons.forEach(tab => {
-    // tab.ariaSelected = false;
-    tab.setAttribute('aria-selected', false);
-  });
-  event.currentTarget.setAttribute('aria-selected', true);
-   //finding associate tab panel
-  const { id } = event.currentTarget;
-  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
-  console.log(tabPanel);
-  tabPanel.hidden = false;
-}
+//    tabButtons.forEach(button => {
+//     button.addEventListener('click', handleTabClick);
+//   });
+//   event.currentTarget.setAttribute('aria-selected', true);
+//   tabButtons.forEach(tab => {
+//     // tab.ariaSelected = false;
+//     tab.setAttribute('aria-selected', false);
+//   });
+//   event.currentTarget.setAttribute('aria-selected', true);
+//    //finding associate tab panel
+//   const { id } = event.currentTarget;
+//   const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+//   console.log(tabPanel);
+//   tabPanel.hidden = false;
+// }
 
