44cfea2630a146bc5405ce020a749b10e6fa076e
sh2-106angel <sh2-106.angel@protonmail.com>
2025-06-22 21:21:01 +0800

    Update pictureviewer.js

--- gallery/pictureviewer.js
+++ gallery/pictureviewer.js
@@ -8,14 +8,14 @@ const videoSource = document.querySelector('#video-source');
 const description = document.querySelector('#description');
 
 const fileDescriptions = {
-    "Beautiful-Clouds.JPG" : "I had never seen clouds this pretty or defined before in my life. I was crossing the road, making my way to my usual cafe to study and thankfully, I remembered to look at the sky. I look at the sky often now, unlike before",
-    "Aquarium.MP4" : "Please ignore the noise in the background.. This was my second visit to the Science Centre after several years. The first time I visited was with someone irreplaceable. There was so much to see back then, but everything was replaced in the end.",
-    "Keychain.JPG": "My keychain on my bag that I carry with me everywhere! The cat from Kiki's Delivery Service! My first Ghibli film - and first film ever - was Spirited Away.",
-    "Cape-Leadwort.JPG": "The day I discovered my new favourite flower. By the way, my favourite colour has always been blue!",
-    "Feral-Kitty.JPG": "I see him getting fed by an elderly woman sometimes. I wonder if he lets her pet him. It's rare to see a feral cat in a neighbourhood. One time, I witnessed a cat fight between him and the tabby cat, and I saw his white fur flying in the air in the midst of the brawl. Cats are terrifying sometimes.. ",
-    "Me-Carrying-Flowers.JPG": "Right outside the cafe I studied at, there was a little flower stand, and I liked to handpick floral arrangements for my room. It was really fun putting them together. I still keep the wilted flowers, in a cup on my bookshelf. I think I will throw them now.",
-    "Stamp-Collection.AVIF": "Their temporary new home is an old puzzle box.",
-    "Favourite-Stamp.AVIF": "'Utazás a holdba és a hold körül': Journey to and around the Moon."};
+    "CLOUDS.JPG" : "I had never seen clouds this pretty or defined before in my life. I was crossing the road, making my way to my usual cafe to study and thankfully, I remembered to look at the sky. I look at the sky so much more now than before. 11 March 2025",
+    "AQUARIUM.MP4" : "This was my second visit to this place after several years. The first time I visited was with someone irreplaceable. There was so much to see back then, but everything was replaced in the end. 8 May 2024",
+    "CAPE-LEADWORT.JPG": "The day I discovered my new favourite flower. By the way, my favourite colour has always been blue!",
+    "OLD-STAMPS.AVIF": "Their temporary new home is an old puzzle box.",
+    "FAVOURITE-STAMP.AVIF": "Translated from Hungarian: 'Journey to and around the Moon.'",
+    "LILYPADS.AVIF": "21 June 2025",
+    "SWAN-LAKE.AVIF": "21 June 2025",
+    "PAINTING.AVIF": "21 June 2025"};
 
 
 // Function: Changing title bar when different item is selected in dropdown menu
